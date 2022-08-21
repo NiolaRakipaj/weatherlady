543c60d501ca536c8bd70dced53f26157b428d47
Niola Rakipaj <niolarakipaj@Niolas-MacBook-Pro.local>
2022-08-21 11:17:25 +0200

    Initial commit

---
+++ src/main/java/grupi/dy/weatherlady/WeatherladyApplication.java
@@ -0,0 +1,13 @@
+package grupi.dy.weatherlady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherladyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherladyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupi/dy/weatherlady/WeatherladyApplicationTests.java
@@ -0,0 +1,13 @@
+package grupi.dy.weatherlady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherladyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
